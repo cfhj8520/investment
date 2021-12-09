2b974d9203e427f424ab61f92420ebc2532c4a52
cfhj8520 <cfhj8520@naver.com>
2021-12-09 18:57:59 +0900

    잡일 : 필요없는 import 제거

--- src/main/java/com/fastcampus/investment/api/ProductsRepository.java
+++ src/main/java/com/fastcampus/investment/api/ProductsRepository.java
@@ -3,6 +3,9 @@ package com.fastcampus.investment.api;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ProductsRepository extends JpaRepository<Products, Long> {
 }
