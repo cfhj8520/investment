a9d7f020be85e87f6ceac528f66b78c98ab63e86
cfhj8520 <cfhj8520@naver.com>
2021-12-09 18:45:12 +0900

    추가 : 투자자 수, 모집된 투자 금액 컬럼 추가

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -5,6 +5,8 @@ CREATE TABLE products
     id                     bigint       NOT NULL AUTO_INCREMENT, --id
     title                  varchar(100) NOT NULL,                --상품명
     total_investing_amount bigint       NOT NULL,                --총 투자 모집금액
+    invested_count         bigint       DEFAULT 0,               --투자자 수
+    invested_amount        bigint       DEFAULT 0,               --모집된 투자 금액
     started_at             datetime     NOT NULL,                --투자시작일시
     finished_at            datetime     NOT NULL,                --투자종료일시
     PRIMARY KEY (id)
