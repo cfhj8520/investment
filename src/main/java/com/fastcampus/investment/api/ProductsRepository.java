f621e22a843af7fd6ef74b451e367f008359ac23
cfhj8520 <cfhj8520@naver.com>
2021-12-09 17:31:26 +0900

    수정 : ApiRepository -> ProductsRepository로 이름 변경

--- src/main/java/com/fastcampus/investment/api/ProductsRepository.java
+++ src/main/java/com/fastcampus/investment/api/ProductsRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ApiRepository extends JpaRepository<Products, Long> {
+public interface ProductsRepository extends JpaRepository<Products, Long> {
 }
